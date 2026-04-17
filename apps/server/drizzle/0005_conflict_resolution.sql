f3188c9ff9cf6c591083d5c64e9f69776a54ca46
Brandon Miller <millbj92@gmail.com>
2026-04-17 18:15:00 +0000

    chore: trigger migration workflow for 0005 conflict_resolution columns

--- apps/server/drizzle/0005_conflict_resolution.sql
+++ apps/server/drizzle/0005_conflict_resolution.sql
@@ -4,3 +4,4 @@ ALTER TABLE memories ADD COLUMN IF NOT EXISTS conflict_resolution text;
 ALTER TABLE memories ADD COLUMN IF NOT EXISTS resolved_at timestamp with time zone;
 
 CREATE INDEX IF NOT EXISTS memories_conflict_status_idx ON memories(tenant_id, conflict_status);
+-- trigger migration workflow Fri Apr 17 18:15:00 UTC 2026
